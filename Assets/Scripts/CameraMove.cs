c15afba202a8918a3689faa95995a5ac12674439
ConnorOfLocke <clocke555@gmail.com>
2015-02-25 16:01:16 +1100

    + Added Base Project

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public GameObject FollowObject;
+	Vector3 CurPosition;
+	float yDistance = 0.0f;
+
+	private Vector3 Velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		CurPosition = FollowObject.transform.position;
+		Velocity = Vector3.zero;
+		yDistance = FollowObject.transform.position.y - transform.position.y;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 followPos = FollowObject.transform.position;
+		followPos.z = transform.position.z;
+		followPos.y -= yDistance;
+
+		Vector3 newPostion = Vector3.SmoothDamp(transform.position,
+		                                        followPos,
+		                                        ref Velocity,
+		                                        Time.deltaTime);
+		transform.position = newPostion;
+	}
+}
---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+
+	public float Speed = 5.0f;
+	public float ZSpeed = 3.0f;
+	
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newPos = transform.position;
+		if (Input.GetKey (KeyCode.LeftArrow) )
+		{
+			newPos.x += Speed * Time.deltaTime;
+		}
+		
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			newPos.x -= Speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			newPos.z -= ZSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow))
+		{
+			newPos.z += ZSpeed * Time.deltaTime;
+		}
+
+		transform.position = newPos;
+	}
+}
