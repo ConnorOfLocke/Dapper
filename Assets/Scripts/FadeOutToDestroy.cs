d712aee0c21d8c966f633e195576dae41d0abd5f
Connor Locke-Warburton <clocke555@gmail.com>
2015-03-07 17:43:56 +1100

    + Added Basic Start Screen (No Functionality)

---
+++ Assets/Scripts/FadeOutToDestroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOutToDestroy : MonoBehaviour {
+
+	public float TimeToExist = 1.0f;
+	private float Timer;
+	
+	void Start(){
+		Timer = TimeToExist;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Image>() != null)
+		{
+			Color prevColor = GetComponent<Image>().color;
+			GetComponent<Image>().color = new Color(prevColor.r,
+													prevColor.g,
+													prevColor.b,
+													prevColor.a * (Timer / TimeToExist));
+		}
+		
+		Timer -= Time.deltaTime;
+		if (Timer <= 0)
+			Destroy(this.gameObject);
+	
+	}
+}
