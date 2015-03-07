cb6d7f16e48196e72f64ecbc153ec12c59429760
Connor Locke-Warburton <clocke555@gmail.com>
2015-03-07 16:57:39 +1100

    + Added Enemy Health Bars that fade out in inactivity ~ Improved enemy attack to actually point towards player when attacking

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,60 +6,92 @@ public class HealthBar : MonoBehaviour {
 
 	public Entity AttachedEntity;
 	public float TimeToChange = 0.5f;
+	public bool FadeIfInactive = false;
+	public float TimeToFade = 5.0f;
 
 	private Color originalColor;
 
 	private Vector3 originalPosition;
+	private Vector3 originalScale;
 	private float startHealth;
 	
-	private float AdjustedTimeToChange;
-	
+	private float AdjustedTimeToScale;
+	private float CurTimeToFade;
 	void Start ()
 	{
 		startHealth = AttachedEntity.Health;
 		originalPosition = transform.localPosition;
-		AdjustedTimeToChange = TimeToChange;
+		originalScale = transform.localScale;
+		
+		AdjustedTimeToScale = TimeToChange;
 		originalColor = GetComponent<Image>().color;
+		
+		CurTimeToFade = 0;
 	}
 	
 	void Update () {
 		float healthScale = AttachedEntity.Health / startHealth;
-		if (transform.localScale.x != healthScale)
+		//compare scales w/ rouding errors in check
+		if (Mathf.Round((transform.localScale.x) * 1000) != Mathf.Round(healthScale * originalScale.x * 1000))
 		{
+		
 			Vector3 Velocity = Vector3.zero;
+			//scales the health bar
 			GetComponent<RectTransform>().localScale = Vector3.SmoothDamp( GetComponent<RectTransform>().localScale,
-																			new Vector3( AttachedEntity.Health / startHealth,1,1),
-																			ref Velocity,
-			                                                              	AdjustedTimeToChange);
-			if (AdjustedTimeToChange > 0.01)
-				AdjustedTimeToChange -= Time.deltaTime;
+			                                                              new Vector3( healthScale * originalScale.x, originalScale.y , originalScale.z),
+																		   ref Velocity,
+			                                                               AdjustedTimeToScale);
+			//speeds it up as it changes
+			if (AdjustedTimeToScale > 0.01)
+				AdjustedTimeToScale -= Time.deltaTime;
 			else		
-				AdjustedTimeToChange = 0.01f;		
+				AdjustedTimeToScale = 0.01f;		
+				
+			//resets the colour
+			GetComponent<Image>().color = originalColor;
 			
+			//jitters a random amount
 			Vector2 jitter = Random.insideUnitSphere * 10;
 			GetComponent<RectTransform>().localPosition = new Vector3 ( originalPosition.x + jitter.x,
 			                                                           originalPosition.y + jitter.y,
-			                                                           originalPosition.z);			
+			                                                           originalPosition.z);
+			//resets the alpha cuz somthing happened                                                        	
+			if (FadeIfInactive)
+				CurTimeToFade = TimeToFade;
 		}
 		else
 		{
-			if (AttachedEntity.Health / startHealth < 0.2f)
+			if (healthScale < 0.2f)
 			{
+				//jitters the position and flashs red in below %20 health
 				GetComponent<Image>().color = Color.Lerp(Color.red, originalColor, (Mathf.Sin(2 * Time.time) + 1.0f) * 0.5f);
 				Vector2 jitter = Random.insideUnitSphere * 10;
 				GetComponent<RectTransform>().localPosition = new Vector3 ( originalPosition.x + jitter.x,
 				                                                           originalPosition.y + jitter.y,
-				                                                           originalPosition.z);			
+				                                                        	originalPosition.z);
+				//resets the alpha cuz somthing happened                                                        	
+				if (FadeIfInactive)
+					CurTimeToFade = TimeToFade;
+				
 			}
 			else
 			{
-				GetComponent<Image>().color = originalColor;
+				//resets the color
+				if (FadeIfInactive)
+				{
+					GetComponent<Image>().color = new Color(originalColor.r,
+					                                        originalColor.g,
+					                                        originalColor.b,
+					                                        CurTimeToFade / TimeToFade);
+					CurTimeToFade -= Time.deltaTime;
+				}
+				else
+					GetComponent<Image>().color = originalColor;
+
 				GetComponent<RectTransform>().localPosition = originalPosition;
+				
 			}
-			AdjustedTimeToChange = TimeToChange;
+			AdjustedTimeToScale = TimeToChange;
 		}
-		
-		
-		
 	}
 }
